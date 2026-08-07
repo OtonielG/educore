424071226624c99dd2a1939e6b48bede8927f711
OtonielG <otonielgomez993@gmail.com>
2026-08-07 16:08:36 -0600

    feat: update padding and layout structure in Dashboard for improved spacing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,10 +3,10 @@ import Stats from "./components/ui/stats";
 
 export default function Dashboard() {
   return (
-    <section className="w-full flex-1 min-h-0 flex flex-col">
+    <section className="w-full flex flex-col p-5 lg:flex-1 lg:min-h-0">
       <Stats />
-      <div className="w-full flex-1 min-h-0 flex flex-col md:flex-row">
-        <div className="w-full lg:w-2/3 p-5">
+      <div className="w-full flex flex-col md:flex-row lg:flex-1 lg:min-h-0">
+        <div className="w-full lg:w-2/3">
           <CountChart />
         </div>
         <div className="w-full lg:w-1/3"></div>
