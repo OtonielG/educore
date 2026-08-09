9b8901de34c7cbb3b9a78bce69406029adcdc21d
OtonielG <otonielgomez993@gmail.com>
2026-08-09 01:14:31 -0600

    feat: refactor StudentsList to use Redux for student data selection

--- src/app/dashboard/estudiantes/components/students-list.tsx
+++ src/app/dashboard/estudiantes/components/students-list.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { useState } from "react";
-import { STUDENTS_PER_PAGE, students } from "@/src/features/students";
+import { useSelector } from "react-redux";
+import { STUDENTS_PER_PAGE } from "@/src/features/students";
+import { selectStudents } from "@/src/store/students/selectors";
 import StudentListHeader from "./student-list-header";
 import StudentListItem from "./student-list-item";
 import StudentsPagination from "./students-pagination";
 
 export default function StudentsList() {
   const [currentPage, setCurrentPage] = useState(1);
+  const students = useSelector(selectStudents);
 
   const totalPages = Math.ceil(students.length / STUDENTS_PER_PAGE);
   const startIndex = (currentPage - 1) * STUDENTS_PER_PAGE;
