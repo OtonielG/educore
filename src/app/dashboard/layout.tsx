de9a6db5788a12710b408595e74d928943e63ba7
OtonielG <otonielgomez993@gmail.com>
2026-08-08 23:08:31 -0600

    feat: integrate Redux Toolkit for state management and update student mock data

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import type { ReactNode } from "react";
+import { StoreProvider } from "@/src/store/provider";
 import Menu from "./components/layout/menu";
 import Navbar from "./components/layout/navbar";
 import "./dashboard.css";
@@ -11,24 +12,26 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div className="dashboard flex min-h-svh overflow-hidden">
-      <aside className="w-1/6 max-w-[240px]">
-        <Link href="/dashboard">
-          <Image
-            src="/educore-logo.avif"
-            alt="Educore Logo"
-            width={780}
-            height={160}
-            className="hidden w-full max-w-[160px] px-2 py-3 sm:block"
-          />
-        </Link>
-        <Menu />
-      </aside>
+    <StoreProvider>
+      <div className="dashboard flex min-h-svh overflow-hidden">
+        <aside className="w-1/6 max-w-[240px]">
+          <Link href="/dashboard">
+            <Image
+              src="/educore-logo.avif"
+              alt="Educore Logo"
+              width={780}
+              height={160}
+              className="hidden w-full max-w-[160px] px-2 py-3 sm:block"
+            />
+          </Link>
+          <Menu />
+        </aside>
 
-      <main className="bg-dashboard-background min-w-0 grow overflow-x-hidden overflow-y-auto flex flex-col">
-        <Navbar />
-        {children}
-      </main>
-    </div>
+        <main className="bg-dashboard-background min-w-0 grow overflow-x-hidden overflow-y-auto flex flex-col">
+          <Navbar />
+          {children}
+        </main>
+      </div>
+    </StoreProvider>
   );
 }
